cf41671acedddc9b3d80054a75a97d9478da109a
Hashem Sami <hashem.sf@gmail.com>
2021-04-04 05:10:20 +0300

    first commit

---
+++ src/Pages/home-page/HomePage.component.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { HomePageContainer } from "./HomePage.styles";
+
+const HomePage: FC = () => {
+  return <HomePageContainer>test</HomePageContainer>;
+};
+
+export default HomePage;
