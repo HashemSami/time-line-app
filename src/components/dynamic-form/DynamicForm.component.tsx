04ef6b1114d4f65b88725b699a5f2ebc728bb906
Hashem <hashem.sf@gmail.com>
2021-04-27 20:41:10 +0300

    added radio dynamic form

--- src/components/dynamic-form/DynamicForm.component.tsx
+++ src/components/dynamic-form/DynamicForm.component.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useState, Fragment } from "react";
 import { DynamicFormContainer, FormGroup, FormGroupRadio, FormActions, FormTitle } from "./DynamicForm.styles";
 
 import { FormModel, RedioProps } from "../../models";
@@ -15,13 +15,13 @@ interface DynamicFormProps {
 // }
 
 const DynamicForm: FC<DynamicFormProps> = ({ title = "Dynamic Form", model, onSubmit, getOnChangeValues }) => {
-  const [formValues, setFormValues] = useState<{ [key: string]: string | number } | null>();
+  const [formValues, setFormValues] = useState<{ [key: string]: string | number } | null>({});
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>, key: string) => {
+  const handleOnChange = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>, key: string) => {
     // console.log(e.target.value);
 
     setFormValues(state => {
-      return { ...state, [key]: e.target.value };
+      return { ...state, [key]: value };
     });
 
     if (getOnChangeValues) {
@@ -30,24 +30,17 @@ const DynamicForm: FC<DynamicFormProps> = ({ title = "Dynamic Form", model, onSu
   };
 
   const renderForm = () => {
+    if (!formValues) {
+      return;
+    }
     const formUI = model.map(modelElement => {
       const { key, label, element, props } = modelElement;
       const { type = "text" } = props;
       const target = key;
-      if (!formValues) {
-        return;
-      }
+
       const value = formValues[target];
 
       let input: JSX.Element | JSX.Element[] = <div></div>;
-
-      // if (element === "input") {
-      //   input = <input key={key} {...props} type={type} value={value} onChange={e => handleOnChange(e, key)} />;
-      // }
-
-      // if (element === "radio") {
-      //   (modelElement as RedioProps).options.map(() => console.log());
-      // }
 
       switch (element) {
         case "input":
@@ -55,11 +48,15 @@ const DynamicForm: FC<DynamicFormProps> = ({ title = "Dynamic Form", model, onSu
           break;
         case "radio":
           input = (modelElement as RedioProps).options.map(({ key, label, name, value }) => (
-            <>
+            <Fragment key={`fr${key}`}>
               <input key={key} {...props} type={element} name={name} value={value} onChange={e => handleOnChange(e, name)} />
               <label key={`ll${key}`}>{label}</label>
-            </>
+            </Fragment>
           ));
+          input = <FormGroupRadio>{input}</FormGroupRadio>;
+          break;
+
+        case "select":
       }
 
       return (
